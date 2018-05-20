ee2c5d078e6acfcfdf7db40583879dc3585461df
Jane C Anaekwe <chiomimo15@gmail.com>
2018-05-20 21:08:17 +0100

    [#157564047] Created endpoint for getting all requests

---
+++ test/src/index.spec.js
@@ -0,0 +1,8 @@
+// import { assert } from 'chai';
+// import first from '../../src/index';
+
+// describe('Index', () => {
+//   it('first should return hello world', () => {
+//     assert.equal(first(), 'hello world');
+//   });
+// });
