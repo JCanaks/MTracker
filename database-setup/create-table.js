9a1818563b2188f3c60a693277b7d011ae6c7b8c
Jane C Anaekwe <chiomimo15@gmail.com>
2018-05-31 19:11:46 +0100

    chore(horeku) create db tables on heroku
     create sql file for setting up tables on heroku
    
     [Delivers #158005896]

--- database-setup/create-table.js
+++ database-setup/create-table.js
@@ -4,17 +4,10 @@ pool.connect((error, client, done) => {
   if (error) {
     console.log(`not able to get connection ${error}`);
   }
-  client.query('CREATE TABLE userAccount( "userId" serial PRIMARY KEY NOT NULL, "userFullname" text NOT NULL, role text NOT NULL,"userPassword" character(60) NOT NULL, department text NOT NULL, "userEmail" text NOT NULL, "userPhonenumber" text NOT NULL)', (queryError) => {
-    done();
-    if (queryError) {
-      console.log(queryError);
-    }
-  });
+  client.query('CREATE TABLE "userAccount"( "userId" serial PRIMARY KEY NOT NULL, "userFullname" text NOT NULL, role text NOT NULL,"userPassword" character(60) NOT NULL, department text NOT NULL, "userEmail" text NOT NULL, "userPhonenumber" text NOT NULL)');
 
-  client.query('CREATE TABLE request( "requestId" serial PRIMARY KEY NOT NULL, "userId" integer NOT NULL, description text NOT NULL,department text NOT NULL,"requestType" text NOT NULL, "requestedBy" text NOT NULL,"requestLevel" text NOT NULL,"requestStatus" text NOT NULL, "requestDate" text NOT NULL, CONSTRAINT "request_userId_fkey" FOREIGN KEY ("userId") REFERENCES public."userAccount" ("userId") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION)', (queryError) => {
+  client.query('CREATE TABLE request( "requestId" serial PRIMARY KEY NOT NULL, "userId" integer NOT NULL, description text NOT NULL,department text NOT NULL,"requestType" text NOT NULL, "requestedBy" text NOT NULL,"requestLevel" text NOT NULL,"requestStatus" text NOT NULL, "requestDate" text NOT NULL, CONSTRAINT "request_userId_fkey" FOREIGN KEY ("userId") REFERENCES public."userAccount" ("userId") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION)', () => {
     done();
-    if (queryError) {
-      console.log(queryError);
-    }
   });
 });
+
