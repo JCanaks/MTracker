1696301daccf1ed5fe06bdc324ff06390de2b6c9
JCanaks <chiomimo15@gmail.com>
2018-06-11 03:25:07 +0100

    chore(apidocs) create api docs for app
     - created api endpoint docs for app using swagger
    - added swagger json file to app
    - created endpoint to serve swagger json file
    
    [Delivers #158241447]

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 import user from './routes/user';
 import admin from './routes/admin';
+
 
 const app = express();
 
 app.use('/api/v1/auth', user);
 app.use('/api/v1/users', user);
 app.use('/api/v1/requests', admin);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Welcome To Maintenace Tracker!');
